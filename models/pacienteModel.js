47187e1cc1c12276dd8e87a220f70a936292dff2
Lautaro-10 <laucadelago123@gmail.com>
2025-05-21 17:50:55 -0300

    paciente Modelo

---
+++ models/pacienteModel.js
@@ -0,0 +1,13 @@
+const db = require('../database/connection');
+function obtenerPacientes(callback) {
+  db.query('SELECT * FROM pacientes', (error, resultados) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, resultados); 
+    }
+  });
+}
+module.exports = {
+  obtenerPacientes
+};
