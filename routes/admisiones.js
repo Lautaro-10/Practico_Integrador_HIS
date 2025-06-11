e64d37d5fb7f580f444d3a69bcfbbff1deeb2241
Lautaro-10 <laucadelago123@gmail.com>
2025-06-11 12:45:59 -0300

    Admisiones.js

--- routes/admisiones.js
+++ routes/admisiones.js
@@ -4,4 +4,25 @@ const admisionesController = require('../controllers/admisionesController');
 
 router.get('/', admisionesController.listarAdmisiones);
 
+router.get('/nueva', async (req, res) => {
+  try {
+    const pacientes = await Paciente.findAll();
+    res.render('nuevaAdmision', { pacientes });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error al cargar pacientes');
+  }
+});
+
+router.post('/nueva', async (req, res) => {
+  const { fecha, hora, motivo, pacienteId } = req.body;
+  try {
+    await Admisiones.create({ fecha, hora, motivo, pacienteId });
+    res.redirect('/admisiones');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error al guardar admisión');
+  }
+});
+
 module.exports = router;
